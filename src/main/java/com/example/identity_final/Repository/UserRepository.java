f0ea04556ef2db0c78c916e9cc6e2595f518ef58
NguyettMiinh <hinatavolshouyou@gmail.com>
2025-06-05 15:28:15 +0700

    check exist by user name when create new user

--- src/main/java/com/example/identity_final/Repository/UserRepository.java
+++ src/main/java/com/example/identity_final/Repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //jpa se tu dong generate 1 cau query kiem tra su ton tai cua username
+    boolean existsByUsername(String username);
 }
