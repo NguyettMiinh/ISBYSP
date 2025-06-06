3c1265531acf28b0e5328ef70f9b8e203508840f
NguyettMiinh <hinatavolshouyou@gmail.com>
2025-06-06 12:09:09 +0700

    create function generate Token: create header and payload for JWT

--- src/main/java/com/example/identity_final/Service/AuthenticationService.java
+++ src/main/java/com/example/identity_final/Service/AuthenticationService.java
@@ -5,6 +5,10 @@ import com.example.identity_final.dto.request.AuthenticationRequest;
 import com.example.identity_final.dto.response.AuthenticationResponse;
 import com.example.identity_final.exception.AppException;
 import com.example.identity_final.exception.ErrorCode;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -12,6 +16,10 @@ import org.aspectj.weaver.ast.Var;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +44,27 @@ public class AuthenticationService {
             throw new AppException(ErrorCode.UNAUTHENTICATED);
         }
 
+
+    }
+
+    //tao method generate token: chi can thong tin username o thoi diem hien tai
+    private String generateToken(String username) {
+        //jwt co 3 phan: header, payload, sign
+        //tao header: voi thuat toan hs512: de bam
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        //tao payload voi nhung thong tin can thiet
+        //DATA TRONG  body dung claim
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("nguyetminh.com") //token dc issuer tu dai: tu domain
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli() //het han trong 1 tieng
+                )) //xac dinh tgian het han token
+                .claim("customClaim","custom")
+                .build();
+        //jwt object can 2 param: header va payload
+        JWSObject jwsObject = new JWSObject();
 
     }
 }
