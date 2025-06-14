78c58332096d67e4262a84b82371e251f22ab9fe
NguyettMiinh <hinatavolshouyou@gmail.com>
2025-06-14 12:18:39 +0700

    done config oauth2 jwt for get method

--- src/main/java/com/example/identity_final/config/SecurityConfig.java
+++ src/main/java/com/example/identity_final/config/SecurityConfig.java
@@ -1,34 +1,52 @@
 package com.example.identity_final.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 //noi cau hinh he thong
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final String[] PUBLIC_ENDPOINT = {
-            "/users","/auth/login","/auth/introspect"
+    private final String[] PUBLIC_ENDPOINTS = {"/users",
+            "/auth/login", "/auth/introspect"
     };
-
+    @Value("${jwt.signerKey}")
+    private String signerKey;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        //nhung endpoint can bao ve, nhung endpoint ko can bao ve ma cho qua luon
-        //dung method de cofig nhung request
         httpSecurity.authorizeHttpRequests(request ->
-                //cho phep truy cap ko can security
-                request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT).permitAll()
-                        //con lai phai xac thuc
+                request.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
                         .anyRequest().authenticated());
-        //tat csrf
-        httpSecurity.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 
 }
