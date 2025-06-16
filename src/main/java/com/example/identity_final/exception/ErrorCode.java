fc1ce70b219c79b1064a92dbe4aa326b1edce0d2
NguyettMiinh <hinatavolshouyou@gmail.com>
2025-06-16 13:48:21 +0700

    Thêm bien httpStatusCode o file enum ErrorCode voi kieu du lieu HttpStatusCode

--- src/main/java/com/example/identity_final/exception/ErrorCode.java
+++ src/main/java/com/example/identity_final/exception/ErrorCode.java
@@ -2,19 +2,22 @@ package com.example.identity_final.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
     //khai bao enum, no la hang so, ma hang nay la 1 object co 2 thuoc tinh
-    USER_EXIST(401,"user has been existed."),
-    UNCATEGORIZED_EXCEPTION(999, "uncategorized exception"),
-    USERNAME_INVALID(403,"username must be at least 5 characters"),
-    PASSWORD_INVALID(404,"Password must be at least 8 characters"),
-    INVALID_KEY(405,"Invalid key."),
-    USER_NOT_EXISTED(406,"user not existed"),
-    UNAUTHENTICATED(407, "unauthenticated");
+    USER_EXIST(401,HttpStatus.BAD_REQUEST,"user has been existed."),
+    UNCATEGORIZED_EXCEPTION(999, HttpStatus.INTERNAL_SERVER_ERROR, "uncategorized exception"),
+    USERNAME_INVALID(403,HttpStatus.BAD_REQUEST,"username must be at least 5 characters"),
+    PASSWORD_INVALID(404,HttpStatus.BAD_REQUEST,"Password must be at least 8 characters"),
+    INVALID_KEY(405, HttpStatus.BAD_REQUEST,"Invalid key."),
+    USER_NOT_EXISTED(406,HttpStatus.NOT_FOUND,"user not existed"),
+    UNAUTHENTICATED(407,HttpStatus.UNAUTHORIZED, "unauthenticated");
     //define error code and message
     private int code;
+    private HttpStatusCode httpStatusCode;
     private String message;
 }
